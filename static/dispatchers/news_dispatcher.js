91ca23fa12ca6b1e92088067ad217e7d80b432eb
okamurayasuyuki <haradashinya@gmail.com>
2013-03-11 02:08:38 +0900

    fix

--- static/dispatchers/news_dispatcher.js
+++ static/dispatchers/news_dispatcher.js
@@ -1,7 +1,6 @@
 var NewsDispatcher = function(em){
     var self = this;
 
-    console.log(em);
     em.on("render",function(d){
         var newsCollectionView = NewsCollectionView(em);
         newsCollectionView.render(d);
