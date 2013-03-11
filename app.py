abc34e67d30cdc36c22426eda735a5516329ed99
okamurayasuyuki <haradashinya@gmail.com>
2013-03-11 22:54:38 +0900

    a

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import os
 from flask import Flask,render_template
+from werkzeug.contrib.fixers import ProxyFix
 
 
 app = Flask(__name__)
@@ -7,8 +8,9 @@ app = Flask(__name__)
 @app.route("/")
 def hello():
     return render_template("index.html")
+app.wsgi_app = ProxyFix(app.wsgi_app)
 
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT",8000))
+    port = int(os.environ.get("PORT",5000))
     app.run(host="127.0.0.1",port=port)
